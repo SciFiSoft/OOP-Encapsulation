08284118b98051447e25418197281bda1109ad1b
nidhro <amdadcse13@gmail.com>
2018-11-16 13:40:25 +0600

    Demo package with interface practice

---
+++ NetBeansProjects/AbstractionPractice/src/Interface/Demo.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Interface;
+
+/**
+ *
+ * @author nidhro
+ */
+public class Demo implements MyInterface {
+
+    public void myMethod1(){
+        System.out.println("Method 1 is called");
+    }
+    public void myMethod2(){
+        System.out.println("Method 2 is called");
+        
+    }
+    public static void main(String[] args) {
+      Demo obj=new Demo();
+      obj.myMethod1();
+      obj.myMethod2();
+    }
+}
